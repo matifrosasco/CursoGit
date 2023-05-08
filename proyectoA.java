efbe0e849290fda32499bc205b51c135c8fff9f9
matifrosasco <132936042+matifrosasco@users.noreply.github.com>
2023-05-08 16:36:07 -0300

    Update proyectoA.java

--- proyectoA.java
+++ proyectoA.java
@@ -5,7 +5,7 @@ public class proyectoA {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Mi primer Git llamado: proyecto A ");
-		
+		System.out.println("Hago una modificacion para probar PULL");
 	
 
 	}
