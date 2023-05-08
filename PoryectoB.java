bffa7c524fbd783b5db41e4fff83d9616409fafd
matifrosasco <mati.frosasco@gmail.com>
2023-05-08 16:24:40 -0300

    Cambio nombre a poryectos

--- PoryectoB.java
+++ PoryectoB.java
@@ -1,8 +1,11 @@
 
-public class ProyectoB {
+public class PoryectoB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Este es el proyecto B ");
+		
 
 	}
 
--- proyectoA.java
+++ proyectoA.java
@@ -4,20 +4,9 @@ public class proyectoA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Mi primer Git");
+		System.out.println("Mi primer Git llamado: proyecto A ");
 		
-		int num=5;
-		int num2=10;
-		
-		System.out.println(" La suma es: " + (num+num2));
-		
-		String nombre="Matias";
-		System.out.println("Mi nombre es: " + nombre);
-		
-		System.out.println("Mi name es: " + nombre);
-		
-		
-		
+	
 
 	}
 
